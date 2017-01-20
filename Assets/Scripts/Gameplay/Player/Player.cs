941ca49e75e9f02e24a1a5a784f115c01fa1c38f
Sebastian Sacher <bastisacher@freenet.de>
2017-01-20 22:01:43 +0100

    ADDED begin with player movement

---
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float Speed = 4;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * Speed * Time.deltaTime);
+    }
+}
