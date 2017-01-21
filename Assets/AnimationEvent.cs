6c3a18aca34283674b19d7ba7de0359b6e8f5849
Sebastian Sacher <bastisacher@freenet.de>
2017-01-21 13:57:27 +0100

    ADDED began with animation events and water waves

---
+++ Assets/AnimationEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour {
+
+    public GameObject WaveParticlesPrefab;
+
+    public Transform LeftFootTransform;
+    public Transform RightFootTransform;
+
+    private Vector3 spawnPos = Vector3.zero;
+
+    public void LeftFoot ()
+    {
+        spawnPos = LeftFootTransform.position;
+        spawnPos.y = 0;
+
+        Instantiate(WaveParticlesPrefab, spawnPos, Quaternion.identity);
+	}
+
+    public void RightFoot()
+    {
+        spawnPos = RightFootTransform.position;
+        spawnPos.y = 0;
+
+        Instantiate(WaveParticlesPrefab, spawnPos, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Gameplay/Skills/Vibration.cs
+++ Assets/Scripts/Gameplay/Skills/Vibration.cs
@@ -23,6 +23,10 @@ public class Vibration : Skill
 
     public override void DoUpdate()
     {
+        // dont vibrate if no controller connected
+        if (GamePad.GetState(base.player.GetPlayerIndex()).IsConnected == false)
+            return;
+        
         // check distance between players 
         Player otherPlayer = PlayerManager.Instance.GetPlayerWithNotNumber(player.PlayerNumber);
         float distance = Vector3.Distance(player.transform.position, otherPlayer.transform.position);
