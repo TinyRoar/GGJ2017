7df849f332debdb6d6b97ba9166e9056f03f09da
Dario <mailme@dariodomi.de>
2017-01-21 14:51:31 +0100

    ADDED ScriptedAnimation script

---
+++ Assets/Scripts/TinyRoar/Framework/UI/Components/ScriptedAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedAnimation : MonoBehaviour {
+
+    public Vector3 Position;
+    public Vector3 Rotation;
+    public Vector3 Scale;
+
+    void Update () {
+        this.transform.position += Position * Time.deltaTime;
+        this.transform.rotation = Quaternion.Euler(
+            this.transform.rotation.x + Rotation.x * Time.deltaTime,
+            this.transform.rotation.y + Rotation.y * Time.deltaTime,
+            this.transform.rotation.z + Rotation.z * Time.deltaTime
+        );
+        this.transform.localScale += Scale * Time.deltaTime;
+    }
+}
