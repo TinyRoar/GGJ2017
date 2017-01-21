c40e19df7e2ed7f0add120c4f206fc875edd4777
Awesomaurice <maurice@superawesome.de>
2017-01-22 00:24:33 +0100

    ADDED wall Vibration

---
+++ Assets/Scripts/Gameplay/Player/WallVibration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class WallVibration : MonoBehaviour {
+
+    public Player _player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // do vibration oneshot
+        if (_player == null)
+            _player = this.transform.parent.GetComponent<Player>();
+
+        float vibrationValue = 0.1f;
+        GamePad.SetVibration(_player.GetPlayerIndex(), vibrationValue, vibrationValue);
+
+    }
+
+}
