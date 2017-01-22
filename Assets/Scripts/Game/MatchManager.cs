c00211659293759937855da37bbf5149b7f2d267
Dario <mailme@dariodomi.de>
2017-01-22 13:53:30 +0100

    ADDED menu controls via controller

--- Assets/Scripts/Game/MatchManager.cs
+++ Assets/Scripts/Game/MatchManager.cs
@@ -60,7 +60,7 @@ public class MatchManager : Singleton<MatchManager>
 
                 // UI
                 UIManager.Instance.Switch(Layer.InGame, UIAction.Hide);
-                UIManager.Instance.Switch(Layer.MainMenu, UIAction.Show);
+                //UIManager.Instance.Switch(Layer.MainMenu, UIAction.Show);
                 UIHandling.Instance.DoEndUI();
 
                 UIManager.Instance.Switch(GameEnvironment.End);
---
+++ Assets/Scripts/Gameplay/UI/ControllerControls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XInputDotNetPure;
+
+public class ControllerControls : MonoBehaviour
+{
+
+    public bool ReactToA = false;
+    public bool ReactToB = false;
+
+    private bool isAllowed = true;
+
+
+    void Update()
+    {
+        if (isAllowed == false)
+            return;
+
+         GamePadState state = GamePad.GetState(PlayerIndex.One);
+
+        if (ReactToA == true)
+        {
+            // Detect if a button was pressed/release this frame
+            if (state.Buttons.A == ButtonState.Pressed)
+                TriggerButton();
+        }
+
+        if (ReactToB == true)
+        {
+            // Detect if a button was pressed/release this frame
+            if (state.Buttons.B == ButtonState.Pressed)
+                TriggerButton();
+        }
+
+    }
+
+    private void TriggerButton()
+    {
+        isAllowed = false;
+        Timer.Instance.Add(5.0f, delegate
+        {
+            isAllowed = true;
+        });
+
+        Debug.Log(test);
+        this.GetComponent<Button>().onClick.Invoke();
+    }
+
+}
